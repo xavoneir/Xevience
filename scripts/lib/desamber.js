295e062e22b9d202f9e8e9e36f1090df72ec9c4f
Conrad Leander <xavoneir@gmail.com>
2018-09-24 22:45:10 -0700

    Add JS lib

---
+++ scripts/lib/desamber.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function Desamber(str)
+{
+  this.str = str.match(/\d\d[a-z\+]\d\d/i) ? str : "01+01";
+
+  this.y = str.substr(0,2);
+  this.m = str.substr(2,1).toUpperCase();
+  this.d = str.substr(3,2);
+
+  this.year = parseInt(`20${this.y}`);
+  this.month = this.m == "+" ? 26 : this.m.charCodeAt(0) - 65;
+  this.doty = (parseInt(this.month) * 14) + parseInt(this.d);
+
+  this.date = new Date(this.year, 0).setDate(this.doty)
+  this.offset = parseInt((this.date - new Date())/86400000)
+
+  this.to_gregorian = function()
+  {
+    let d = this.to_date()
+    return `${d.getFullYear()}-${prepend(d.getMonth()+1,2)}-${prepend(d.getDate(),2)}`;
+  }
+
+  this.to_date = function()
+  {
+    return new Date(this.date);
+  }
+
+  this.ago = function(cap = 9999)
+  {
+    let days = this.offset;
+
+    if(-days > cap)  { return `${this.toString()}`; }
+
+    if(days == -1)   { return `yesterday`; }
+    if(days == 1)    { return "tomorrow"; }
+    if(days == 0)    { return "today"; }
+    if(days < -365)  { return `${parseInt(days/-365)} years ago`; }
+    if(days < 1)     { return `${days*-1} days ago`; }
+    return `in ${days} days`;
+  }
+
+  this.toString = function()
+  {
+    return this.str.toUpperCase();
+  }
+
+  function prepend(s,l,c="0"){ while(`${s}`.length < l){ s = `${c}${s}`; }; return s; }
+}
+
+Date.prototype.desamber = function()
+{
+  let year = this.getFullYear()
+  let start = new Date(year, 0, 0);
+  let diff = (this - start) + ((start.getTimezoneOffset() - this.getTimezoneOffset()) * 60 * 1000);
+  let doty = Math.floor(diff/86400000);
+  let leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)
+  let days = leap ? 366 : 365
+
+  let y = year.toString().substr(2,2);
+  let m = String.fromCharCode(97 + Math.floor(((doty)/days) * 26)).toUpperCase(); m = doty == 365 || doty == 366 ? "+" : m;
+  let d = (doty % 14); d = d < 10 ? `0${d}` : d; d = d == "00" ? "14" : d; d = doty == 365 ? "01" : (doty == 366 ? "02" : d);
+  
+  return new Desamber(`${y}${m}${d}`);
+}
---
+++ scripts/lib/runic.js
@@ -0,0 +1,104 @@
+'use strict';
+
+function Runic(lines = [],templater = null)
+{
+  this.lines = lines;
+
+  const runes = {
+    "&":{tag:"p"},
+    "?":{tag:"p",class:"note"},
+    "-":{tag:"ln",wrapper:"list"},
+    "#":{tag:"ln",wrapper:"code"},
+    "*":{tag:"h3"},
+    "+":{tag:"hs"},
+    "@":{tag:"quote",fn:quote},
+    "|":{tag:"tr",wrapper:"table",fn:table},
+    "%":{fn:media},
+    ">":{}, 
+  }
+
+  function is_runic(l)
+  {
+    let rune = l.substr(0,1);
+    let trail = l.substr(1,1);
+
+    if(trail != " "){ console.warn("Non-Runic",l); return false; }
+    if(!runes[rune]){ console.warn(`Non-Runic[${rune}]`,l); return false; }
+
+    return true;
+  }
+
+  function stash(acc,l)
+  {
+    let rune = l.substr(0,1)
+    let line = l.substr(2)
+    let prev = acc[acc.length-1] ? acc[acc.length-1] : [{rune:rune,a:[]}]
+
+    if(prev.rune == rune){
+      prev.a.push(line)
+    }
+    else{
+      acc.push({rune:rune,a:[line]})
+    }
+
+    return acc
+  }
+
+  function _html(acc,stash)
+  {
+    let html = ""
+    let wr = runes[stash.rune].wrapper
+    for(let id in stash.a){
+      let r = runes[stash.rune]
+      let txt = r.fn ? r.fn(stash.a[id]) : stash.a[id]
+      let htm = templater ? new templater(txt) : txt
+      html += r.tag ? `<${r.tag} class='${r.class ? r.class : ''}'>${htm}</${r.tag}>` : `${htm}`
+    }
+    return wr ? `${acc}<${wr}>${html}</${wr}>` : `${acc}${html}`
+  }
+
+  // Templates
+
+  function quote(content)
+  {
+    let parts = content.split(" | ")
+    let text = parts[0].trim()
+    let author = parts[1]
+    let source = parts[2]
+    let link = parts[3]
+
+    return `
+      ${text.length > 1 ? `<p class=\'text\'>${text}</p>` : ''}
+      ${author ? `<p class='attrib'>${author}${source && link ? `, <a href='${link}'>${source}</a>` : source ? `, <b>${source}</b>` : ''}</p>` : ''}`
+  }
+
+  function media(content)
+  {
+    let service = content.split(" ")[0];
+    let id = content.split(" ")[1];
+
+    if(service == "itchio"){ return `<iframe frameborder="0" src="https://itch.io/embed/${id}?link_color=000000" width="600" height="167"></iframe>`; }
+    if(service == "bandcamp"){ return `<iframe style="border: 0; width: 600px; height: 274px;" src="https://bandcamp.com/EmbeddedPlayer/album=${id}/size=large/bgcol=ffffff/linkcol=333333/artwork=small/transparent=true/" seamless></iframe>`; }
+    if(service == "youtube"){ return `<iframe width="100%" height="380" src="https://www.youtube.com/embed/${id}?rel=0" style="max-width:700px" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`; }
+    if(service == "custom"){ return `<iframe src='${id}' style='width:100%;height:350px;'></iframe>`; }
+    return content.indexOf(" | ") > -1 ? gallery(content.split(" | ")) : `<img src='media/${service}' class='${id}'/>`;
+  }
+
+  function gallery(content)
+  {
+    return `<div class="gallery">${content.map(media)}</div>`;
+  }
+
+  function table(content)
+  {
+    return `<td>${content.trim().replace(/ \| /g,"</td><td>")}</td>`
+  }
+
+  //
+
+  this.toString = function()
+  {
+    let lines = this.lines.filter(is_runic);
+    return lines.reduce(stash,[]).reduce(_html,"");
+  }
+}
